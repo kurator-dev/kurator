81c2851ae38573b7eff7d3b17008f99389e2ec07
guoyao <guoyao17@huawei.com>
2022-06-22 15:25:37 +0800

    fix make gen-check jb not found
    Signed-off-by: guoyao <guoyao17@huawei.com>

--- hack/gen-prom.sh
+++ hack/gen-prom.sh
@@ -4,6 +4,15 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+REPO_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd -P)"
+
+source "${REPO_ROOT}/hack/util.sh"
+
+# install prerequisite tools
+util::install_tools github.com/jsonnet-bundler/jsonnet-bundler/cmd/jb latest
+util::install_tools github.com/brancz/gojsontoyaml latest
+util::install_tools github.com/google/go-jsonnet/cmd/jsonnet latest
+
 echo 'begin to generate prom manifests'
 echo "path: $1";
 echo "version: $2"
