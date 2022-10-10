6413370595ee96fec5570ec424e71c5b44895e1b
hejianpeng <hejianpeng2@huawei.com>
2022-10-10 17:37:06 +0800

    fix local-dev-down.sh
    Signed-off-by: hejianpeng <hejianpeng2@huawei.com>

--- hack/local-dev-down.sh
+++ hack/local-dev-down.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+# shellcheck disable=SC2046
 set -o errexit
 set -o nounset
 set -o pipefail
 
-kind delete clusters "$(kind get clusters | grep kurator)"
+kind delete clusters $(kind get clusters | grep kurator)
