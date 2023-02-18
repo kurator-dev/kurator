87fcb7e386a58dd74de0a98f8ebb578610e1a816
hejianpeng <hejianpeng2@huawei.com>
2023-02-18 17:49:25 +0800

    docs: publish to Github Pages
    Signed-off-by: hejianpeng <hejianpeng2@huawei.com>

---
+++ hack/local-docsite-build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# shellcheck disable=SC2164
+
+HUGO_BASE_URL=${HUGO_BASE_URL:-"https://kurator.dev"}
+export HUGO_ENV="production"
+export HUGO_ENVIRONMENT="production"
+
+pushd docs
+    npm install --no-save
+    hugo --minify --baseURL "${HUGO_BASE_URL}" --destination ../out/public
+popd
