46f13c17f1ee8cc19c606c485d4c7e13f7fffb6c
hejianpeng <hejianpeng2@huawei.com>
2023-02-27 20:13:06 +0800

    use exclusive CloudFormation stack per cluster
    Signed-off-by: hejianpeng <hejianpeng2@huawei.com>

---
+++ pkg/infra/scope/cluster_test.go
@@ -0,0 +1,41 @@
+/*
+Copyright Kurator Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package scope
+
+import (
+	"testing"
+
+	. "github.com/onsi/gomega"
+	"k8s.io/apimachinery/pkg/types"
+)
+
+func TestStack(t *testing.T) {
+	c := &Cluster{
+		UID: "dcf78fcbb",
+		NamespacedName: types.NamespacedName{
+			Name:      "quickstart",
+			Namespace: "default",
+		},
+	}
+
+	gotSuffix := c.StackSuffix()
+	gotStackName := c.StackName()
+
+	g := NewWithT(t)
+	g.Expect(gotSuffix).To(Equal(".default-quickstart-dcf78fcbb.cluster.kurator.dev"))
+	g.Expect(gotStackName).To(Equal("default-quickstart-dcf78fcbb-cluster-kurator-dev"))
+}
