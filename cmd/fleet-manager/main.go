9e4fff5e7243ff9ded3095ffe57da940b5e9714a
Zhonghu Xu <xuzhonghu@huawei.com>
2023-03-27 17:45:56 +0800

    Init fleet-manager
    Signed-off-by: Zhonghu Xu <xuzhonghu@huawei.com>

---
+++ cmd/fleet-manager/main.go
@@ -0,0 +1,144 @@
+/*
+Copyright Kurator Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+	_ "net/http/pprof"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
+	cgrecord "k8s.io/client-go/tools/record"
+	"k8s.io/klog/v2"
+	"k8s.io/klog/v2/klogr"
+	"sigs.k8s.io/cluster-api/util/record"
+	ctrl "sigs.k8s.io/controller-runtime"
+
+	"kurator.dev/kurator/cmd/fleet-manager/options"
+	"kurator.dev/kurator/cmd/fleet-manager/scheme"
+	"kurator.dev/kurator/pkg/fleet-manager"
+	"kurator.dev/kurator/pkg/util"
+	"kurator.dev/kurator/pkg/version"
+)
+
+var log = ctrl.Log.WithName("fleet-manager")
+
+func main() {
+	klog.InitFlags(nil)
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+
+	cmd := newRootCommand()
+	if err := cmd.Execute(); err != nil {
+		fmt.Println("execute fleet-manager command failed: ", err)
+		os.Exit(-1)
+	}
+}
+
+func newRootCommand() *cobra.Command {
+	opts := &options.Options{}
+	cmd := &cobra.Command{
+		Use:          "fleet-manager",
+		Short:        "fleet-manager is in charge of fleet management.",
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			ctrl.SetLogger(klogr.New())
+			util.PrintFlags(log, cmd.Flags())
+			ctx := ctrl.SetupSignalHandler()
+			return run(ctx, opts)
+		},
+	}
+	cmd.AddCommand(newVersionCommand())
+
+	cmd.ResetFlags()
+
+	opts.AddFlags(cmd.PersistentFlags())
+
+	return cmd
+}
+
+func newVersionCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "version",
+		Short: "Print the version of kurator fleet-manager",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			v := version.Get()
+			y, err := json.MarshalIndent(&v, "", "  ")
+			if err != nil {
+				return err
+			}
+			fmt.Println(string(y))
+			return nil
+		},
+	}
+	return cmd
+}
+
+func run(ctx context.Context, opts *options.Options) error {
+
+	if opts.ProfilerAddress != "" {
+		log.Info("Profiler listening for requests", "profiler-address", opts.ProfilerAddress)
+		go func() {
+			log.Error(http.ListenAndServe(opts.ProfilerAddress, nil), "listen and serve error")
+		}()
+	}
+
+	// Machine and cluster operations can create enough events to trigger the event recorder spam filter
+	// Setting the burst size higher ensures all events will be recorded and submitted to the API
+	broadcaster := cgrecord.NewBroadcasterWithCorrelatorOptions(cgrecord.CorrelatorOptions{
+		BurstSize: 100,
+	})
+
+	restConfig := ctrl.GetConfigOrDie()
+	restConfig.UserAgent = "kurator-fleet-manager"
+	mgr, err := ctrl.NewManager(restConfig, ctrl.Options{
+		Scheme:                     scheme.Scheme,
+		MetricsBindAddress:         opts.MetricsBindAddr,
+		LeaderElection:             opts.EnableLeaderElection,
+		LeaderElectionResourceLock: resourcelock.LeasesResourceLock,
+		LeaderElectionID:           "kurator-fleet-manager-leader-elect",
+		LeaderElectionNamespace:    opts.LeaderElectionNamespace,
+		EventBroadcaster:           broadcaster,
+		HealthProbeBindAddress:     opts.HealthAddr,
+	})
+	if err != nil {
+		log.Error(err, "unable to create manager")
+		return err
+	}
+
+	record.InitFromRecorder(mgr.GetEventRecorderFor("cluster-operator"))
+	if err := (&fleet.FleetManager{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		log.Error(err, "unable to set up fleet manager", "controller", "fleet manager")
+		return err
+	}
+
+	log.Info("starting manager", "version", version.Get().String())
+	if err := mgr.Start(ctx); err != nil {
+		log.Error(err, "running manager error")
+		return err
+	}
+
+	return nil
+}
---
+++ pkg/fleet-manager/clusters.go
@@ -0,0 +1,187 @@
+/*
+Copyright Kurator Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package fleet
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	join "github.com/karmada-io/karmada/pkg/karmadactl/join"
+	"github.com/karmada-io/karmada/pkg/karmadactl/options"
+	"github.com/karmada-io/karmada/pkg/karmadactl/unjoin"
+	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/types"
+	restclient "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	clusterv1alpha1 "kurator.dev/kurator/pkg/apis/cluster/v1alpha1"
+	fleetapi "kurator.dev/kurator/pkg/apis/fleet/v1alpha1"
+)
+
+func (f *FleetManager) reconcileClusters(ctx context.Context, fleet *fleetapi.Fleet) (ctrl.Result, error) {
+	fleetKey := client.ObjectKeyFromObject(fleet)
+	log := ctrl.LoggerFrom(ctx).WithValues("fleet", fleetKey)
+	var unreadyClusters int32
+	var result ctrl.Result
+	var readyClusters []clusterv1alpha1.Cluster
+	// Loop over cluster, and add labels to the cluster
+	for _, cluster := range fleet.Spec.Clusters {
+		// cluster namespace can be not set, always use fleet namespace as a fleet can only include clusters in the same namespace.
+		clusterKey := types.NamespacedName{Name: cluster.Name, Namespace: fleet.Namespace}
+		var cluster clusterv1alpha1.Cluster
+		err := f.Get(ctx, clusterKey, &cluster)
+		if err != nil {
+			if !apierrors.IsNotFound(err) {
+				log.Error(err, "unable to fetch cluster", "cluster", clusterKey)
+				return result, err
+			}
+			log.Info("cluster not found yet", "cluster", clusterKey)
+			result.RequeueAfter = RequeueAfter
+		} else {
+			// label the cluster
+			if cluster.Labels == nil {
+				cluster.Labels = make(map[string]string)
+			}
+			if cluster.Labels[FleetLabel] != fleet.Name {
+				cluster.Labels[FleetLabel] = fleet.Name
+				err = f.Update(ctx, &cluster)
+				if err != nil {
+					log.Error(err, "unable to label cluster", "cluster", clusterKey)
+					return ctrl.Result{}, err
+				}
+			}
+			// Register the ready cluster to the control plane
+			if cluster.Status.Phase == string(clusterv1alpha1.ClusterPhaseReady) {
+				readyClusters = append(readyClusters, cluster)
+			} else {
+				unreadyClusters++
+			}
+		}
+	}
+
+	fleet.Status.ReadyClusters = int32(len(readyClusters))
+	fleet.Status.UnReadyClusters = unreadyClusters
+
+	var kubeconfig corev1.Secret
+	err := f.Get(ctx, fleetKey, &kubeconfig)
+	if err != nil {
+		return result, err
+	}
+
+	controlplaneRestConfig, err := clientcmd.RESTConfigFromKubeConfig(kubeconfig.Data["kubeconfig"])
+	if err != nil {
+		log.Error(err, "build restconfig for controlplane failed")
+		return result, fmt.Errorf("build restconfig for controlplane failed %v", err)
+	}
+	for _, cluster := range readyClusters {
+		err := f.joinCluster(ctx, controlplaneRestConfig, &cluster)
+		if err != nil {
+			log.Error(err, "Join cluster failed")
+			return result, err
+		}
+	}
+
+	// TODO: cleanup labels once the cluster is removed from the fleet
+
+	return result, nil
+}
+
+func (f *FleetManager) reconcileClustersOnDelete(ctx context.Context, fleet *fleetapi.Fleet) (ctrl.Result, error) {
+	log := ctrl.LoggerFrom(ctx).WithValues("fleet", types.NamespacedName{Name: fleet.Name, Namespace: fleet.Namespace})
+	var result ctrl.Result
+	// Loop over cluster, and add labels to the cluster
+	for _, cluster := range fleet.Spec.Clusters {
+		// cluster namespace can be not set, always use fleet namespace as a fleet can only include clusters in the same namespace.
+		clusterKey := types.NamespacedName{Name: cluster.Name, Namespace: fleet.Namespace}
+		var cluster clusterv1alpha1.Cluster
+		err := f.Get(ctx, clusterKey, &cluster)
+		if err != nil {
+			if !apierrors.IsNotFound(err) {
+				log.Error(err, "unable to fetch cluster", "cluster", clusterKey)
+				return result, err
+			}
+			log.Info("cluster not found maybe deleted or not created", "cluster", clusterKey)
+		} else {
+			if cluster.Labels[FleetLabel] == fleet.Name {
+				delete(cluster.Labels, FleetLabel)
+				err = f.Update(ctx, &cluster)
+				if err != nil {
+					log.Error(err, "unable to remove cluster label", "cluster", clusterKey)
+					return result, err
+				}
+			}
+		}
+	}
+
+	return result, nil
+}
+
+// ClusterKubeconfigDataName is the key used to store a Kubeconfig in the secret's data field.
+const ClusterKubeconfigDataName = "value"
+
+func (f *FleetManager) joinCluster(ctx context.Context, controlPlane *restclient.Config, cluster *clusterv1alpha1.Cluster) error {
+	var secret corev1.Secret
+	secretKey := types.NamespacedName{Name: cluster.Status.KubeconfigSecretRef, Namespace: cluster.Namespace}
+
+	if err := f.Get(ctx, secretKey, &secret); err != nil {
+		return fmt.Errorf("get secret %v for cluster %v failed %v", secretKey, client.ObjectKeyFromObject(cluster), err)
+	}
+	clusterKubeconfig := secret.Data[ClusterKubeconfigDataName]
+	clusterRestConfig, err := clientcmd.RESTConfigFromKubeConfig(clusterKubeconfig)
+	if err != nil {
+		return fmt.Errorf("build restconfig for cluster %v failed %v", client.ObjectKeyFromObject(cluster), err)
+	}
+
+	option := join.CommandJoinOption{
+		ClusterNamespace: options.DefaultKarmadaClusterNamespace,
+		ClusterName:      cluster.Name,
+	}
+	err = option.RunJoinCluster(controlPlane, clusterRestConfig)
+	if err != nil {
+		return fmt.Errorf("join cluster %v failed %v", client.ObjectKeyFromObject(cluster), err)
+	}
+	return nil
+}
+
+func (f *FleetManager) unjoinCluster(ctx context.Context, controlPlane *restclient.Config, cluster *clusterv1alpha1.Cluster) error {
+	var secret corev1.Secret
+	secretKey := types.NamespacedName{Name: cluster.Status.KubeconfigSecretRef, Namespace: cluster.Namespace}
+
+	if err := f.Get(ctx, secretKey, &secret); err != nil {
+		return fmt.Errorf("get secret %v for cluster %v failed %v", secretKey, client.ObjectKeyFromObject(cluster), err)
+	}
+	clusterKubeconfig := secret.Data[ClusterKubeconfigDataName]
+	clusterRestConfig, err := clientcmd.RESTConfigFromKubeConfig(clusterKubeconfig)
+	if err != nil {
+		return fmt.Errorf("build restconfig for cluster %v failed %v", client.ObjectKeyFromObject(cluster), err)
+	}
+
+	option := unjoin.CommandUnjoinOption{
+		ClusterNamespace: options.DefaultKarmadaClusterNamespace,
+		ClusterName:      cluster.Name,
+		Wait:             60 * time.Second,
+	}
+	err = option.RunUnJoinCluster(controlPlane, clusterRestConfig)
+	if err != nil {
+		return fmt.Errorf("unjoin cluster %v failed %v", client.ObjectKeyFromObject(cluster), err)
+	}
+	return nil
+}
