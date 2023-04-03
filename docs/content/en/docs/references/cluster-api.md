77eaf70dd4e554b10b744e2c4bdfbcd19b6fffee
hejianpeng <hejianpeng2@huawei.com>
2023-04-03 19:33:13 +0800

    docs: add api docs
    Signed-off-by: hejianpeng <hejianpeng2@huawei.com>

---
+++ docs/content/en/docs/references/cluster-api.md
@@ -0,0 +1,8 @@
+---
+title: "Cluster API reference"
+linkTitle: "Cluster API"
+weight: 10
+description: "Detailed docuementation on the clusters API"
+---
+
+{{< reference cluster_types >}}
--- hack/tool/tool.go
+++ hack/tool/tool.go
@@ -4,6 +4,7 @@
 package tools
 
 import (
+	_ "github.com/elastic/crd-ref-docs"
 	_ "k8s.io/code-generator"
 	_ "sigs.k8s.io/controller-runtime/tools/setup-envtest"
 	_ "sigs.k8s.io/controller-tools/cmd/controller-gen"
