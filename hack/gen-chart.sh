3297c618edb6883dc8334e1afba39aff258a00b6
Zhonghu Xu <xuzhonghu@huawei.com>
2023-04-07 15:06:31 +0800

    Added gen chart for fleet manager
    Signed-off-by: Zhonghu Xu <xuzhonghu@huawei.com>

--- hack/gen-chart.sh
+++ hack/gen-chart.sh
@@ -13,7 +13,7 @@ rm -rf "${OUT_BASE_PATH}"/charts
 mkdir -p "${OUT_BASE_PATH}"/charts
 
 MAINIFESTS_CHART_PATH=${REPO_ROOT}/manifests/charts
-HELM_CHARTS=(cluster-operator)
+HELM_CHARTS=(cluster-operator fleet-manager)
 HELM_CHART_VERSION=${HELM_CHART_VERSION:-"0.1.0"}
 IMAGE_HUB=${IMAGE_HUB:-"ghcr.io/kurator-dev"}
 IMAGE_TAG=${IMAGE_TAG:-"latest"}
