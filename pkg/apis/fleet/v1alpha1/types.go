c62040323e15953ed2ba61df5f805d5fed722467
hejianpeng <hejianpeng2@huawei.com>
2023-05-10 19:39:28 +0800

    fleet: init monitoring api
    Signed-off-by: hejianpeng <hejianpeng2@huawei.com>

--- pkg/apis/fleet/v1alpha1/types.go
+++ pkg/apis/fleet/v1alpha1/types.go
@@ -18,7 +18,9 @@ package v1alpha1
 
 import (
 	corev1 "k8s.io/api/core/v1"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	clusterv1alpha1 "kurator.dev/kurator/pkg/apis/cluster/v1alpha1"
 )
 
 type FleetPhase string
@@ -64,6 +66,153 @@ type FleetSpec struct {
 
 	// TODO: support cluster selector?
 	// TODO: add options to allow customize fleet control plane if neccessary. And in future this could not be karmada.
+
+	// Plugin defines the plugins that would be installed in the fleet.
+	// +optional
+	Plugin *PluginConfig `json:"plugin,omitempty"`
+}
+
+type PluginConfig struct {
+	// Metric defines the configuration for the monitoring system installation and metrics collection..
+	// +optional
+	Metric *MetricConfig `json:"metric,omitempty"`
+	// Grafana defines the configuration for the grafana installation and observation.
+	// +optional
+	Grafana *GrafanaConfig `json:"grafana,omitempty"`
+}
+
+type MetricConfig struct {
+	// Thanos defines the configuration for the thanos querier and store gateway.
+	Thanos ThanosConfig `json:"thanos,omitempty"`
+	// Prometheus defines the configuration for the prometheus installation
+	// in the clusters observed by the thanos,
+	// by default thanos sidecar will be installed in thanos sidecar mode.
+	Prometheus PrometheusConfig `json:"prometheus,omitempty"`
+}
+
+type PrometheusConfig struct {
+	// Chart defines the helm chart config of the prometheus.
+	// default values is
+	//
+	// chart:
+	//   repository: oci://registry-1.docker.io/bitnamicharts
+	//   name: kube-prometheus
+	//   version: 8.9.1
+	//
+	// +optional
+	Chart *ChartConfig `json:"chart,omitempty"`
+	// NodeExporter defines the configuration for the node exporter.
+	// +optional
+	NodeExporter *PrometheusExporterConfig `json:"nodeExporter,omitempty"`
+	// KubeStateMetrics defines the configuration for the kube-state-metrics.
+	// +optional
+	KubeStateMetrics *PrometheusExporterConfig `json:"kubeStateMetrics,omitempty"`
+	// ExtraArgs is the set of extra arguments for Prometheus chart.
+	//
+	// For Example, using following configuration to create a ServiceMonitor to monitor prometheus itself.
+	// extraArgs:
+	//   prometheus:
+	//     serviceMonitor:
+	//       enabled: true
+	//
+	// +optional
+	ExtraArgs apiextensionsv1.JSON `json:"extraArgs,omitempty"`
+}
+
+type PrometheusExporterConfig struct {
+	// Enabled indicates whether the exporters are enabled.
+	Enabled bool `json:"enabled,omitempty"`
+}
+
+type ChartConfig struct {
+	// Repository defines the repository of chart.
+	// Default value depends on the kind of the component.
+	// +optional
+	Repository string `json:"repository,omitempty"`
+	// Name defines the name of the chart.
+	// Default value depends on the kind of the component.
+	// +optional
+	Name string `json:"name,omitempty"`
+	// Version defines the version of the chart.
+	// Default value depends on the kind of the component.
+	// +optional
+	Version string `json:"version,omitempty"`
+}
+
+type ThanosConfig struct {
+	// Chart defines the helm chart config of the thanos.
+	// default values is
+	//
+	// chart:
+	//   repository: oci://registry-1.docker.io/bitnamicharts
+	//   name: thanos
+	//   version: 12.5.1
+	//
+	// +optional
+	Chart *ChartConfig `json:"chart,omitempty"`
+	// Mode defines the mode of the Thanos.
+	// The default is sidecar.
+	// +kubebuilder:validation:Enum=sidecar;reciever
+	// +optional
+	Mode string `json:"mode,omitempty"`
+	// ObjectStoreConfig defines the configuration for the object store.
+	ObjectStore *ThanosObjectStoreConfig `json:"objectStore,omitempty"`
+	// ExtraArgs is the set of extra arguments for Thanos chart.
+	//
+	// For Example, using following configuration to enable query frontend.
+	// extraArgs:
+	//   queryFrontend:
+	//     enabled: true
+	//
+	// +optional
+	ExtraArgs apiextensionsv1.JSON `json:"extraArgs,omitempty"`
+}
+
+type ThanosObjectStoreConfig struct {
+	// Type defines the type of the object store.
+	// Now only support s3, which means S3-compatible object storages, e.g. MinIO.
+	Type string `json:"type"`
+	// S3 defines the configuration for the S3 object store.
+	// For more information, see https://thanos.io/tip/thanos/storage.md/#s3
+	// This field is required if the type is s3.
+	// +optional
+	S3 *S3Config `json:"s3,omitempty"`
+}
+
+type S3Config struct {
+	// Bucket defines the name of the bucket.
+	Bucket string `json:"bucket"`
+	// Region defines the region of the bucket.
+	Region string `json:"region"`
+	// Endpoint defines the endpoint of the bucket.
+	Endpoint string `json:"endpoint"`
+	// Credential is the credential used to access the bukcet.
+	// Make sure the credential is in the same namespace as the monitoring.
+	// The secret must have following keys: AccessKeyID, SecretAccessKey.
+	// SessionToken is optional.
+	Credential clusterv1alpha1.CredentialConfig `json:"credential"`
+}
+
+type GrafanaConfig struct {
+	// Chart defines the helm chart config of the grafana.
+	// default values is
+	//
+	// chart:
+	//   repository: oci://registry-1.docker.io/bitnamicharts
+	//   name: grafana
+	//   version: 8.2.33
+	//
+	// +optional
+	Chart *ChartConfig `json:"chart,omitempty"`
+	// ExtraArgs is the set of extra arguments for Grafana chart.
+	//
+	// For Example, using following configuration to change replica count.
+	// extraArgs:
+	//   grafana:
+	//     replicaCount: 2
+	//
+	// +optional
+	ExtraArgs apiextensionsv1.JSON `json:"extraArgs,omitempty"`
 }
 
 // FleetStatus defines the observed state of the fleet
