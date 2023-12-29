24306763cb817a9401888a893f36bdcdbbf58723
Xieql <xieqianglong@huawei.com>
2023-12-29 16:38:32 +0800

    pipeline api: add volumClaimTemplate (#513)
    * pipeline api: add volumClaimTemplate
    
    Signed-off-by: Xieql <xieqianglong@huawei.com>
    
    * update
    
    Signed-off-by: Xieql <xieqianglong@huawei.com>
    
    ---------
    
    Signed-off-by: Xieql <xieqianglong@huawei.com>

--- pkg/apis/pipeline/v1alpha1/type.go
+++ pkg/apis/pipeline/v1alpha1/type.go
@@ -46,10 +46,39 @@ type PipelineSpec struct {
 	// The tasks will be executed in the order they are listed.
 	Tasks []PipelineTask `json:"tasks"`
 
-	// SharedWorkspace is the name of the PVC. If not specified, a PVC with the Pipeline's name as prefix will be created by default.
+	// SharedWorkspace is the config of the PVC where task using
+	// The PersistentVolumeClaim with this config will be created for each pipeline execution
+	// it allows the user to specify e.g. size and StorageClass for the volume.
 	// If not set, Kurator will create a PVC named Pipeline.name using default config
 	// +optional
-	SharedWorkspace *string `json:"sharedWorkspace,omitempty"`
+	SharedWorkspace *VolumeClaimTemplate `json:"sharedWorkspace,omitempty"`
+}
+
+// VolumeClaimTemplate is the configuration for the volume claim template in pipeline execution.
+// For more details, see https://github.com/kubernetes/api/blob/master/core/v1/types.go
+type VolumeClaimTemplate struct {
+	// AccessMode determines the access modes for the volume, e.g., ReadWriteOnce.
+	// This affects how the volume can be mounted.
+	// "ReadWriteOnce" can be mounted in read/write mode to exactly 1 host
+	// "ReadOnlyMany" can be mounted in read-only mode to many hosts
+	// "ReadWriteMany" can be mounted in read/write mode to many hosts
+	// "ReadWriteOncePod" can be mounted in read/write mode to exactly 1 pod, cannot be used in combination with other access modes
+	AccessMode corev1.PersistentVolumeAccessMode `json:"accessMode,omitempty"`
+
+	// StorageRequest defines the storage size required for this PVC, e.g., 1Gi, 100Mi.
+	// It specifies the storage capacity needed as part of ResourceRequirements.
+	// +kubebuilder:validation:Pattern="^[0-9]+(\\.[0-9]+)?(Gi|Mi)$"
+	StorageRequest string `json:"requestsStorage,omitempty"`
+
+	// StorageClassName specifies the StorageClass name to which this persistent volume belongs, e.g., manual.
+	// It allows the PVC to use the characteristics defined by the StorageClass.
+	StorageClassName string `json:"storageClassName,omitempty"`
+
+	// VolumeMode specifies whether the volume should be used with a formatted filesystem (Filesystem)
+	// or remain in raw block state (Block). The Filesystem value is implied when not included.
+	// "Block"  means the volume will not be formatted with a filesystem and will remain a raw block device.
+	// "Filesystem"  means the volume will be or is formatted with a filesystem.
+	VolumeMode corev1.PersistentVolumeMode `json:"volumeMode,omitempty"`
 }
 
 type PipelineTask struct {
