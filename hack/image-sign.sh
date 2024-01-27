9075db324e330cb7f09920f94471ebbde8ad0d8f
LiZhenCheng9527 <lizhencheng6@huawei.com>
2024-01-27 17:14:00 +0800

    add double quote in image-sign.sh to prevent globbing and word splitting
    Signed-off-by: LiZhenCheng9527 <lizhencheng6@huawei.com>

--- hack/image-sign.sh
+++ hack/image-sign.sh
@@ -7,11 +7,11 @@ SIGN_IMAGE=${SIGN_IMAGE:-"0"}
 CLUSTER_OPERATOR_IMAGE=${CLUSTER_OPERATOR_IMAGE:-"${IMAGE_HUB}/cluster-operator:${IMAGE_TAG}"}
 FLEET_MANAGER_IMAGE=${FLEET_MANAGER_IMAGE:-"${IMAGE_HUB}/fleet-manager:${IMAGE_TAG}"}
 
-if [ $SIGN_IMAGE = "1" ]; then
-    echo "Sign image: "${CLUSTER_OPERATOR_IMAGE}
-    cosign sign --yes ${CLUSTER_OPERATOR_IMAGE}
-    echo "Sign image: "${FLEET_MANAGER_IMAGE}
-    cosign sign --yes ${FLEET_MANAGER_IMAGE}
+if [ "$SIGN_IMAGE" = "1" ]; then
+    echo "Sign image: ""${CLUSTER_OPERATOR_IMAGE}"
+    cosign sign --yes "${CLUSTER_OPERATOR_IMAGE}"
+    echo "Sign image: ""${FLEET_MANAGER_IMAGE}"
+    cosign sign --yes "${FLEET_MANAGER_IMAGE}"
 else
     echo "Warning: The build image is not signed"
 fi
