25b161f0f02c043e621aeeda1c51af9174cbd3ca
wulianglongrd <wulianglongrd@gmail.com>
2024-04-09 19:27:51 +0800

    Fix shell script incompatibility on mac system (#618)
    * Fix mac system compatibility
    
    Signed-off-by: wulianglongrd <wulianglongrd@gmail.com>
    
    * add sleep back
    
    Signed-off-by: wulianglongrd <wulianglongrd@gmail.com>
    
    * fix
    
    Signed-off-by: wulianglongrd <wulianglongrd@gmail.com>
    
    ---------
    
    Signed-off-by: wulianglongrd <wulianglongrd@gmail.com>

--- hack/gen-chart.sh
+++ hack/gen-chart.sh
@@ -8,6 +8,7 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 
 OUT_BASE_PATH=${REPO_ROOT}/out
 CHART_OUT_PATH=${OUT_BASE_PATH}/charts
+HELM_CHART_NAME=${HELM_CHART_NAME:-}
 
 rm -rf "${OUT_BASE_PATH}"/charts
 mkdir -p "${OUT_BASE_PATH}"/charts
@@ -18,14 +19,16 @@ HELM_CHART_VERSION=${HELM_CHART_VERSION:-"0.1.0"}
 IMAGE_HUB=${IMAGE_HUB:-"ghcr.io/kurator-dev"}
 IMAGE_TAG=${IMAGE_TAG:-"latest"}
 
+source "$REPO_ROOT/hack/util.sh"
+
 for c in "${HELM_CHARTS[@]}"
 do
     echo "gen chart $c"
     cp -r "${MAINIFESTS_CHART_PATH}/${c}" "${CHART_OUT_PATH}/${c}"
-    sed -i "s|hub: ghcr.io/kurator-dev|hub: ${IMAGE_HUB}|g" $(find ${CHART_OUT_PATH}/${HELM_CHART_NAME} -type f | grep values.yaml)
-    sed -i "s|tag: latest|tag: ${IMAGE_TAG}|g" $(find ${CHART_OUT_PATH}/${HELM_CHART_NAME} -type f | grep values.yaml)
-    sed -i "s|version: 0.1.0|version: ${HELM_CHART_VERSION}|g" $(find ${CHART_OUT_PATH}/${HELM_CHART_NAME} -type f | grep Chart.yaml)
-    sed -i "s|appVersion: 0.1.0|appVersion: ${HELM_CHART_VERSION}|g" $(find ${CHART_OUT_PATH}/${HELM_CHART_NAME} -type f | grep Chart.yaml)
+    util::sed_in_place "s|hub: ghcr.io/kurator-dev|hub: ${IMAGE_HUB}|g" "$(find ${CHART_OUT_PATH}/${HELM_CHART_NAME} -type f | grep values.yaml)"
+    util::sed_in_place "s|tag: latest|tag: ${IMAGE_TAG}|g" "$(find ${CHART_OUT_PATH}/${HELM_CHART_NAME} -type f | grep values.yaml)"
+    util::sed_in_place "s|version: 0.1.0|version: ${HELM_CHART_VERSION}|g" "$(find ${CHART_OUT_PATH}/${HELM_CHART_NAME} -type f | grep Chart.yaml)"
+    util::sed_in_place "s|appVersion: 0.1.0|appVersion: ${HELM_CHART_VERSION}|g" "$(find ${CHART_OUT_PATH}/${HELM_CHART_NAME} -type f | grep Chart.yaml)"
     helm package "${CHART_OUT_PATH}/${c}" -d "${CHART_OUT_PATH}"
 done
 
--- hack/sync-crds.sh
+++ hack/sync-crds.sh
@@ -12,31 +12,33 @@ AWS_PROVIDER_VERSION=${AWS_PROVIDER_VERSION:-'v2.0.0'}
 # set default ca prevent deleting crd fail
 CA_BUNDLE=${CA_BUNDLE:-'LS0tLS1CRUdJTiBDRVJUSUZJQ0FURS0tLS0tCk1JSUQ0ekNDQXN1Z0F3SUJBZ0lVQ3lxYVFvUktQM1ZtVk52Q0hTazM1UDBnQW5Vd0RRWUpLb1pJaHZjTkFRRUwKQlFBd1hERUxNQWtHQTFVRUJoTUNlSGd4Q2pBSUJnTlZCQWdNQVhneENqQUlCZ05WQkFjTUFYZ3hDakFJQmdOVgpCQW9NQVhneENqQUlCZ05WQkFzTUFYZ3hDekFKQmdOVkJBTU1BbU5oTVJBd0RnWUpLb1pJaHZjTkFRa0JGZ0Y0Ck1CNFhEVEl5TVRFeE1qQTNORFV3TUZvWERUSTNNVEV4TVRBM05EVXdNRm93UmpFWE1CVUdBMVVFQ2hNT2MzbHoKZEdWdE9tMWhjM1JsY25NeEt6QXBCZ05WQkFNTUlpb3VhM1Z5WVhSdmNpMXplWE4wWlcwdWMzWmpMbU5zZFhOMApaWEl1Ykc5allXd3dnZ0VpTUEwR0NTcUdTSWIzRFFFQkFRVUFBNElCRHdBd2dnRUtBb0lCQVFDYThhYk1IVklNCkkxNFp4SDUraHc4SlIwSEVucmlxV0RPMTdTd0hoY21VRHc4emd5d1hwNGdjdTA0SDB6cUFYcTZvNU9hd1lJbFkKSjVsVGloMnBhaGV2K0N5VDhka0pPSURTcE5uMThaZnl2UTkvYVZDdDhiMkJCMTJQcHVwSU1PWEV0dXJ3TFpmMgpCMVlYdzBPQkNJVXVydE96MW9zSlRjZUw1dlUvdkFyNVUvQzN6YVVITFlXTUlQY2tkR2U0dk1FbkdqSjhxUEg0CkxyTUpiZkFnUmZuNmJTQVJQNE01SHYrOVFNTnZjVTY3eWhUcEl0KzlHcU9seG9hbjMzb1J6NnNWeFMwWkFybnEKVm1vWmVZVWJobFhFek5HeXlMQ1UxeWQyY2d0aG1jR1lmb0NwTjFwYW81MUdVWlp5MnRYYWRia3Iraitmdjh0ZQpMOVJrVHgzdzZXaVJBZ01CQUFHamdiSXdnYTh3RGdZRFZSMFBBUUgvQkFRREFnV2dNQjBHQTFVZEpRUVdNQlFHCkNDc0dBUVVGQndNQ0JnZ3JCZ0VGQlFjREFUQU1CZ05WSFJNQkFmOEVBakFBTUIwR0ExVWREZ1FXQkJTYXNuK2cKUEtsdnM2RE9KS2FoWWgydFpyRDlOREFmQmdOVkhTTUVHREFXZ0JTeFBxQWovZXNLV2lpemY5c3EwS3Fxd0hsQQpEekF3QmdOVkhSRUVLVEFuZ2hRcUxtdDFjbUYwYjNJdGMzbHpkR1Z0TG5OMlk0SUpiRzlqWVd4b2IzTjBod1IvCkFBQUJNQTBHQ1NxR1NJYjNEUUVCQ3dVQUE0SUJBUUIydWNuWlBKV1h5WGVkSDBYdEZreHpoTlhUeU0zdkttREEKSWk4TDViZmNVelh4VUVqb1B5aldYRVR0QVNtbTBNS3NUdTdDK3ptalM4QVNOUWZvRjdVcFM0bGNpakc5TXY0RAp1WEdNZnlJTU1hcUUrRWJFR2NJdWVKMEZBMjVZT2tDdzFza1BFVkdVTERZZENVWE1wMDZ1R2ZsR0hiL0JPMm4rCmMvbk5ZdDR4RmtySEhKaUNZM3FXQkhhN0FLVXAxZHU1bnN6Mm4yK3E5WkN0Nmp3YThKaC9zUlZDZFlDNC9tWVkKTWc5VkFjMDZlcTA2dmFWY1pxZU5UbnR1TGFVY3R2KzkvL2NSTzFncS9wbEFFZ015R2V3YUxYWHptcGNlWnV1RwpmbTlzRDdXeHltSlpXaGdEaFAvUmpwZm1rcHNFaGNUcm1leHJFbTJEZ01QVDVwdWhuQmQwCi0tLS0tRU5EIENFUlRJRklDQVRFLS0tLS0K'}
 
+source "$REPO_ROOT/hack/util.sh"
+
 # gen crds
 rm -rf ${HELM_CRD_BASE}/*.x-k8s.io.yaml
 CRD_OUTPUT_DIR=${HELM_CRD_BASE} CLUSTER_API_PROVIDER_VERSION=${CLUSTER_API_PROVIDER_VERSION} AWS_PROVIDER_VERSION=${AWS_PROVIDER_VERSION} go run cmd/crd-gen-tool/main.go
 
 # sed cert
-sed -i "s|caBundle: Cg==|caBundle: ${CA_BUNDLE}|g" $(find ${HELM_CRD_BASE} -type f)
+util::sed_in_place "s|caBundle: Cg==|caBundle: ${CA_BUNDLE}|g" "$(find ${HELM_CRD_BASE} -type f)"
 
 # capi
-sed -i "s|capi-serving-cert|kurator-serving-cert|g" $(find ${HELM_CRD_BASE} -type f)
-sed -i "s|capi-system|{{ .Release.Namespace }}|g" $(find ${HELM_CRD_BASE} -type f)
-sed -i "s|capi-webhook-service|kurator-webhook-service|g" $(find ${HELM_CRD_BASE} -type f)
+util::sed_in_place "s|capi-serving-cert|kurator-serving-cert|g" "$(find ${HELM_CRD_BASE} -type f)"
+util::sed_in_place "s|capi-system|{{ .Release.Namespace }}|g" "$(find ${HELM_CRD_BASE} -type f)"
+util::sed_in_place "s|capi-webhook-service|kurator-webhook-service|g" "$(find ${HELM_CRD_BASE} -type f)"
 
 # bootstrap
-sed -i "s|capi-kubeadm-bootstrap-serving-cert|kurator-serving-cert|g" $(find ${HELM_CRD_BASE} -type f)
-sed -i "s|capi-kubeadm-bootstrap-system|{{ .Release.Namespace }}|g" $(find ${HELM_CRD_BASE} -type f)
-sed -i "s|capi-kubeadm-bootstrap-webhook-service|kurator-webhook-service|g" $(find ${HELM_CRD_BASE} -type f)
+util::sed_in_place "s|capi-kubeadm-bootstrap-serving-cert|kurator-serving-cert|g" "$(find ${HELM_CRD_BASE} -type f)"
+util::sed_in_place "s|capi-kubeadm-bootstrap-system|{{ .Release.Namespace }}|g" "$(find ${HELM_CRD_BASE} -type f)"
+util::sed_in_place "s|capi-kubeadm-bootstrap-webhook-service|kurator-webhook-service|g" "$(find ${HELM_CRD_BASE} -type f)"
 
 # kubeadm-control-plane
-sed -i "s|capi-kubeadm-control-plane-serving-cert|kurator-serving-cert|g" $(find ${HELM_CRD_BASE} -type f)
-sed -i "s|capi-kubeadm-control-plane-system|{{ .Release.Namespace }}|g" $(find ${HELM_CRD_BASE} -type f)
-sed -i "s|capi-kubeadm-control-plane-webhook-service|kurator-webhook-service|g" $(find ${HELM_CRD_BASE} -type f)
+util::sed_in_place "s|capi-kubeadm-control-plane-serving-cert|kurator-serving-cert|g" "$(find ${HELM_CRD_BASE} -type f)"
+util::sed_in_place "s|capi-kubeadm-control-plane-system|{{ .Release.Namespace }}|g" "$(find ${HELM_CRD_BASE} -type f)"
+util::sed_in_place "s|capi-kubeadm-control-plane-webhook-service|kurator-webhook-service|g" "$(find ${HELM_CRD_BASE} -type f)"
 
 # capa
-sed -i "s|capa-serving-cert|kurator-serving-cert|g" $(find ${HELM_CRD_BASE} -type f)
-sed -i "s|capa-system|{{ .Release.Namespace }}|g" $(find ${HELM_CRD_BASE} -type f)
-sed -i "s|capa-webhook-service|kurator-webhook-service|g" $(find ${HELM_CRD_BASE} -type f)
+util::sed_in_place "s|capa-serving-cert|kurator-serving-cert|g" "$(find ${HELM_CRD_BASE} -type f)"
+util::sed_in_place "s|capa-system|{{ .Release.Namespace }}|g" "$(find ${HELM_CRD_BASE} -type f)"
+util::sed_in_place "s|capa-webhook-service|kurator-webhook-service|g" "$(find ${HELM_CRD_BASE} -type f)"
 
 mv ${HELM_CRD_BASE}/*-webhook-configuration.yaml ${WEBHOOK_BASE}
